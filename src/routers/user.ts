67569d38c68cb45820baf4453f3fafccfd85b58a
Tushar <tushar.awate09@gmail.com>
2025-04-12 17:35:59 +0530

    Added refresh token funcationality

--- src/routers/user.ts
+++ src/routers/user.ts
@@ -15,6 +15,8 @@ const users: User[] = [
     { uid: "Sneha", pwd: "4321" },
 ];
 
+let refreshTokens: string[] = [];
+
 router.get("/users", (req: Request, res: Response) => {
     res.send(users);
 });
@@ -43,8 +45,11 @@ router.post("/login", async (req: Request, res: Response) => {
             return res.status(401).send({ error: "User not found" });
         } else {
             if ((await bcrypt.compare(pwd, user.pwd))) {
-                const token = jwt.sign({ uid }, process.env.JWT_ACCESS_TOKEN_SECRET);
-                return res.status(200).send({ message: "Login successful" ,token:token});
+                
+                const token = mintAccessToken(user);                
+                const refreshToken = jwt.sign({ uid: user.uid } , process.env.JWT_REFRESH_ACCESS_TOKEN_SECRET,{expiresIn: '1d'});
+                refreshTokens.push(refreshToken);
+                return res.status(200).send({ message: "Login successful" ,token:token,refreshToken:refreshToken});
             } else {
                 return res.status(401).send({ error: "Invalid password" });
             }
@@ -56,5 +61,27 @@ router.post("/login", async (req: Request, res: Response) => {
             .send({ error: "An error occurred while processing your request." });
     }
 });
+
+router.post("/token", (req: Request, res: Response) => {
+    const refreshToken = req.body.token;
+    if (refreshToken == null) return res.sendStatus(401);
+    console.log(refreshTokens);
+    if(!refreshTokens.includes(refreshToken)) return res.sendStatus(403);
+    jwt.verify(refreshToken, process.env.JWT_REFRESH_ACCESS_TOKEN_SECRET, (err: any, user: any) => {
+        if (err) return res.sendStatus(403);
+        const accessToken = mintAccessToken(user);
+        res.send({ accessToken: accessToken });
+    })
+})
+
+router.delete("/logout", (req: Request, res: Response) => {
+    const refreshToken = req.body.token;    
+    refreshTokens = refreshTokens.filter(token => token !== refreshToken);
+    res.sendStatus(204);    
+})
+function mintAccessToken(user: any) {
+    const accessToken = jwt.sign({ uid: user.uid }, process.env.JWT_ACCESS_TOKEN_SECRET,{expiresIn: '30s'});
+    return accessToken;
+}   
 
 module.exports = router
