dfec1481610d5a412f0ff80a6050652f70e33c73
Tushar Awate <tushar.awate09@gmail.com>
2025-04-15 20:01:50 +0530

    swagger integration

--- src/routers/player.ts
+++ src/routers/player.ts
@@ -6,9 +6,62 @@ const router = express.Router();
 import { Request, Response } from "express";
 
 
-
-
-router.get("/players",verifyAuthToken,async (req: Request, res: Response) => {
+/**
+ * @swagger
+ * /players:
+ *   get:
+ *     summary: Get all Players from IPL 2024
+ *     tags:
+ *       - Players
+ *     parameters:
+ *       - in: header
+ *         name: Authorization
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: Bearer token
+ *     responses:
+ *       200:
+ *         description: List of players
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: object
+ *                 properties:
+ *                   id:
+ *                     type: string
+ *                     example: "1"
+ *                   Name:
+ *                     type: string
+ *                     example: "Virat"
+ *                   Surname:
+ *                     type: string
+ *                     example: "Kohli"
+ *                   Country:
+ *                     type: string
+ *                     example: "Batsman"
+ *                   DOB:
+ *                     type: string
+ *                     example: "1988-11-5"
+ *                   Age:
+ *                     type: number 
+ *                     example: 18
+ *                   Specialism:
+ *                      type: string
+ *                      example: "Batsman"
+ *                   Batting:
+ *                      type: string        
+ *                      example: "Right Handed"                      
+ *                   Bowling:
+ *                      type: string
+ *                      example: "Right Arm"
+ *                   Team:
+ *                       type: string
+ *                       example: "RCB"                   
+ */
+router.get("/players", verifyAuthToken, async (req: Request, res: Response) => {
     const players = await prisma.players.findMany({
         take: 10,
         skip: 1,
